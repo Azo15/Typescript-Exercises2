ae6e289704c61fc4f96f388e35e7c8d4d13bb2ac
Azo İsmail <ismailazo260@gmail.com>
2025-09-22 22:38:07 +0300

    Create loops-in-typescript.ts

---
+++ loops-in-typescript.ts
@@ -0,0 +1,75 @@
+// loops-in-typescript.ts
+
+let MyNumber: number;
+let i: number;
+
+// ************************************************************
+// Kullanıcı 100’den büyük bir sayı girene kadar sorar
+// ************************************************************
+
+// MyNumber = parseInt(prompt("100'den büyük bir sayı giriniz")!);
+
+// while (MyNumber < 100) {
+//   MyNumber = parseInt(prompt("100'den büyük bir sayı giriniz")!);
+// }
+// console.log(MyNumber);
+
+
+// ************************************************************
+// Kullanıcı 1 ile 3 arasında bir sayı girene kadar sorar
+// ************************************************************
+
+// MyNumber = parseInt(prompt("Lütfen 1 ile 3 arasında bir sayı giriniz")!);
+
+// while (MyNumber < 1 || MyNumber > 3) {
+//   MyNumber = parseInt(prompt("Yanlış değer girdiniz, lütfen tekrar deneyiniz")!);
+// }
+// console.log(MyNumber);
+
+
+// ************************************************************
+// Girilen sayının ardından gelen 10 sayıyı yazdırır
+// ************************************************************
+
+// MyNumber = parseInt(prompt("Lütfen bir sayı giriniz...")!);
+
+// for (i = MyNumber + 1; i <= MyNumber + 10; i++) {
+//   console.log(i);
+// }
+
+
+// ************************************************************
+// Girilen sayının çarpım tablosunu yazdırır
+// ************************************************************
+
+// let result: number;
+// MyNumber = parseInt(prompt("Lütfen bir sayı giriniz...")!);
+
+// for (i = 1; i <= 10; i++) {
+//   result = MyNumber * i;
+//   console.log(`${MyNumber} x ${i} = ${result}`);
+// }
+
+
+// ************************************************************
+// Girilen sayıya kadar olan sayıların toplamını bulur
+// ************************************************************
+
+// MyNumber = parseInt(prompt("Lütfen bir sayı giriniz...")!);
+
+// for (i = MyNumber - 1; i >= 0; i--) {
+//   MyNumber = i + MyNumber;
+// }
+// console.log(MyNumber);
+
+
+// ************************************************************
+// Girilen sayının faktöriyelini hesaplar
+// ************************************************************
+
+// MyNumber = parseInt(prompt("Lütfen bir sayı giriniz...")!);
+
+// for (i = MyNumber - 1; i > 0; i--) {
+//   MyNumber = i * MyNumber;
+// }
+// console.log(MyNumber);
